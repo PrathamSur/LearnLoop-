bf98b475d676c3bf877b1a5476063d0e03ef4459
sapyyy <rijusaptadeep@gmail.com>
2025-08-08 20:33:40 +0530

    added some changes in landing page

--- src/Components/LHeader.jsx
+++ src/Components/LHeader.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import LDropdown from './LDropdown' 
+import React from "react";
+import LDropdown from "./LDropdown";
 
 const LHeader = () => {
   return (
-    <div className='bg-[#F2F4F5] h-full flex justify-center items-center md:h-[18%] lg:h-[25%] p-4 lg:p-6'>
-        <div className='bg-white h-full w-[90%] p-2 rounded-3xl flex  justify-between items-center border border-[#aec9d1d5] shadow-lg'>
-            <h1 className='pl-4 text-xl font-bold lg:text-5xl md:text-3xl md:pl-10 lg:pl-20'>LearnLoop</h1>
-            <div className=' hidden lg:flex lg:gap-4 lg:text-lg lg:font-semibold'>
-                <a href="/login" className='text-black hover:text-white hover:bg-black border border-black p-2 rounded-3xl'>Login</a>
-                <a href="/register" className='text-black hover:text-white   hover:bg-black border border-black p-2 rounded-3xl'>Sign Up</a>
-                <a href="/register" className='text-black hover:text-white   hover:bg-black border border-black p-2 rounded-3xl'>Profile</a>
-                <a href="/register" className='text-white bg-red-500 hover:bg-red-700 border-black p-2 rounded-3xl'>Log out</a>
-                
-
-            </div>
-            
-            <LDropdown />
-      
+    <div className="bg-[#F2F4F5] h-full flex justify-center items-center md:h-[18%] lg:h-[25%] p-4 lg:p-6">
+      <div className="bg-white px-5 py-3 h-full w-[90%] p-2 rounded-full flex  justify-between items-center border border-neutral-300 shadow-sm">
+        <h1 className="text-xl font-bold lg:text-2xl md:text-2xl">LearnLoop</h1>
+        <div className="hidden lg:flex lg:gap-2 lg:text-lg lg:font-semibold">
+          <a
+            href="/login"
+            className="text-white bg-black hover:text-black transition duration-300 hover:bg-neutral-50 border border-neutral-900 p-2 rounded-full px-3"
+          >
+            Login
+          </a>
+          <a
+            href="/register"
+            className="text-black hover:text-white transition duration-300 hover:bg-black border border-neutral-900 p-2 rounded-full px-3"
+          >
+            Sign Up
+          </a>
         </div>
 
-
+        <LDropdown />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LHeader
+export default LHeader;
