a398c7ca3b01b08805f32f9b033b51fed9946ca2
PrathamSur <surpratham@gmail.com>
2025-08-26 01:34:55 +0530

    deleted unwanted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import "./index.css";
-import Navbar from "./Components/Navbar";
-import Content from "./Components/Content";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Class from "./pages/Classrooms";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
-import WhiteBoard from "./pages/WhiteBoard";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/classrooms" element={<Class />} />
+        <Route path="/classes" element={<Class />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
--- src/Components/NavContent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NavContent = () => {
-  return (
-    <div className="border rounded-xl border-white p-6 hover:bg-slate-200 md:p-9 md:rounded-3xl sm:p-9"></div>
-  )
-}
-
-export default NavContent
